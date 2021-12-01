fc6b6929b9551440d2af4943c02f9495d5a14d9c
nh-g <giangngohong@gmail.com>
2021-12-01 13:07:43 +0100

    remove unused

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 //Project files
 import "./style/style.sass";
-import BackupData from "./data/products.json";
 import NavigationBar from "./components/NavigationBar";
-import Footer from "./components/Footer";
 import StatusLoading from "./status/StatusLoading";
 import StatusError from "./status/StatusError";
 import StatusLoaded from "./status/StatusLoaded";
@@ -21,8 +19,6 @@ export default function App() {
   // Local states
   const [status, setStatus] = useState(STATUS_LOADING);
   const [data, setData] = useState([]);
-  // Constants
-  const DEBUG_MODE = false;
 
   // Methods
   useEffect(() => {
@@ -32,27 +28,22 @@ export default function App() {
   }, [setData, setStatus]);
 
   function onSuccess(products) {
-    console.log("App.jsx", products);
     setData(products);
     setStatus(STATUS_LOADED);
   }
 
   function onFail() {
-    if (DEBUG_MODE) {
-      setData(BackupData);
-      setStatus(STATUS_LOADED);
-    } else {
-      setStatus(STATUS_ERROR);
-    }
+    setStatus(STATUS_ERROR);
   }
 
   return (
     <div className="App">
       <BrowserRouter>
-        <NavigationBar /> {status === STATUS_LOADING && <StatusLoading />}{" "}
-        {status === STATUS_LOADED && <StatusLoaded data={data} />}{" "}
-        {status === STATUS_ERROR && <StatusError />} <Footer />
-      </BrowserRouter>{" "}
+        <NavigationBar /> 
+        {status === STATUS_LOADING && <StatusLoading />}
+        {status === STATUS_LOADED && <StatusLoaded data={data} />}
+        {status === STATUS_ERROR && <StatusError />} 
+      </BrowserRouter>
     </div>
   );
 }
