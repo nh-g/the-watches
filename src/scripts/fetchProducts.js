3830f21993c5227fe276c5925809b2d65a17c7ab
nh-g <giangngohong@gmail.com>
2021-12-01 13:06:22 +0100

    add debug mode

--- src/scripts/fetchProducts.js
+++ src/scripts/fetchProducts.js
@@ -1,7 +1,13 @@
+// Constants
 const PRODUCTS_API_URL = "https://assignment.dwbt.tech/products";
 const IMAGES_API_URL = "https://assignment.dwbt.tech/images";
+const DEBUG_MODE = false;
 
 export const fetchProducts = async () => {
+  if (DEBUG_MODE) {
+    return require("../data/products.json");
+  }
+
   return Promise.all([
     fetch(PRODUCTS_API_URL).then((response) => response.json()),
     fetch(IMAGES_API_URL).then((response) => response.json()),
